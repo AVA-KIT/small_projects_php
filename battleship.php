9a968afb3a81e045eed14f8fbe84e0980e78a93b
AVA-KIT <gazda.edu@gmail.com>
2020-01-19 13:43:38 +0100

    Usuniecie komentarza

--- battleship.php
+++ battleship.php
@@ -1,8 +1,4 @@
 <?php
-/* It's a a poor version of a strategy game called Battleships or Sea Battle
- By using "argv" can "shots" the "ships" marked in array $ruledGrid
- Run the file like this: php battleship.php A 1
-*/
 
 $ruledGrid = [
         'A' => [0, 0, 0, 0, 0], 
